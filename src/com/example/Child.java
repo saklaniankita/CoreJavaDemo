9317bda208b866faba2584cdc85af0a8a0993e56
Ankita Saklani <saklani.ankita27@gmail.com>
2024-02-15 15:52:47 -0600

    Functional Interfaces in java.util.function package

--- src/com/example/Child.java
+++
@@ -1,5 +0,0 @@
-package com.example;
-
-public class Child extends Parent {
-	int i = 3;
-}
--- src/com/java8/functionalProgramming/BinaryOperatorExample.java
+++ src/com/java8/functionalProgramming/BinaryOperatorExample.java
@@ -3,6 +3,11 @@ package com.java8.functionalProgramming;
 import java.util.List;
 import java.util.function.BinaryOperator;
 
+/**
+ * BinaryOperator is a functional interface in teh package java.util.function
+ *
+ * @author ankitasaklani
+ */
 public class BinaryOperatorExample {
     public static void main(String[] args) {
         List<Integer> agesList = List.of(1, 0, 3, 2, 1, 2, 3);
